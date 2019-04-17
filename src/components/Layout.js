8513aef2aa1a2bd0e58b06fadec76c18d419533b
Thiago de Bastos <thiago@thiagodebastos.com>
2019-04-17 23:11:17 +1000

    add base styles for typography scale and rhythm

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,62 @@ import PropTypes from "prop-types"
 import Header from "./Header"
 
 export const globalStyles = css`
+  html {
+    font-size: 16px;
+  }
+
   body {
+    background-color: white;
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
       Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    -moz-osx-font-smoothing: auto;
+    -webkit-font-smoothing: auto;
+    font-smooth: never;
+    font-weight: 400;
+    line-height: 1.45;
+    color: #333;
+  }
+
+  p {
+    margin-bottom: 1.25em;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5 {
+    margin: 2.75rem 0 1rem;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+    font-weight: 400;
+    line-height: 1.15;
+  }
+
+  h1 {
+    margin-top: 0;
+    font-size: 2.488em;
+  }
+
+  h2 {
+    font-size: 2.074em;
+  }
+
+  h3 {
+    font-size: 1.728em;
+  }
+
+  h4 {
+    font-size: 1.44em;
+  }
+
+  h5 {
+    font-size: 1.2em;
+  }
+
+  small,
+  .text_small {
+    font-size: 0.833em;
   }
 `
 
